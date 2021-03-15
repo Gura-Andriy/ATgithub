2d7200ebc7564b9b2115a124a6b9ccab26e617dc
gura3 <gura3a@gmail.com>
2021-03-15 22:10:46 +0200

    All is good. Now only one stream

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,12 +3,14 @@ public class App {
         MyDriver driver = new MyDriver();
         LoginProperty loginProperty = new LoginProperty();
 
+        //Login on Github
         driver.logIn(loginProperty.username, loginProperty.password);
+        driver.submit("//*[@id=\"login\"]/div[4]/form/div/input[12]", driver.checkTextareaNotEmptyById("password"));
 
-        driver.submit("//*[@id=\"login\"]/div[4]/form/div/input[12]");
-
+        //Click to create new repository
         driver.createNewDirectory();
 
+        //Create repository page
         driver.setNameOfDirectory("TestRepository");
 
         driver.setCheckbox("repository_auto_init");
@@ -17,8 +19,17 @@ public class App {
         driver.setDropDownMenu("//*[@id=\"new_repository\"]/div[6]/div[4]/div[2]/span[2]/details/summary",
                 "//*[@id=\"new_repository\"]/div[6]/div[4]/div[2]/span[2]/details/details-menu/div[3]/div[1]/label[52]/span");
 
-        driver.submit("//*[@id=\"new_repository\"]/div[6]/button");
+        driver.submit("//*[@id=\"new_repository\"]/div[6]/button",  driver.checkTextareaNotEmptyById("repository_name"));
 
+        //Write readme file
+        driver.submit("//*[@id=\"readme\"]/div[1]/div");
+
+        driver.writeText("//*[@id=\"new_blob\"]/div/file-attachment/div/div[2]/div/div/div[5]/div[1]/div/div/div/div[5]/div/pre/span/span",
+                "test text in readme file");
+
+        driver.submitById("submit-file");
+
+        //Delete repository
         driver.delete();
 
         driver.submitDelete();
--- src/main/java/MyDriver.java
+++ src/main/java/MyDriver.java
@@ -18,7 +18,6 @@ public class MyDriver {
     }
 
     public void logIn(String username, String password) {
-        //Log in
         wait.until(ExpectedConditions.elementToBeClickable(By.partialLinkText("Sign in")));
         driver.findElement(By.partialLinkText("Sign in")).click();
         driver.findElement(By.name("login")).sendKeys(username);
@@ -26,18 +25,15 @@ public class MyDriver {
     }
 
     public void createNewDirectory() {
-        //Create new repository
         driver.findElement(By.linkText("New")).click();
     }
 
     public void setNameOfDirectory(String repositoryName) {
-        //Enter name of repository
         wait.until(ExpectedConditions.elementToBeClickable(By.id("repository_name")));
         driver.findElement(By.id("repository_name")).sendKeys(repositoryName);
     }
 
     public void setCheckbox(String id) {
-        //Set checkbox
         wait.until(ExpectedConditions.elementToBeClickable(By.id(id)));
         driver.findElement(By.id(id)).click();
     }
@@ -54,29 +50,48 @@ public class MyDriver {
         driver.findElement(By.xpath(xpath)).click();
     }
 
+    public void submit(String xpath, boolean checkTextarea) {
+        //Click submit bottom if textarea is not empty
+        if (checkTextarea) {
+            submit(xpath);
+        }
+    }
+
+    public void submitById(String id) {
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait.until(ExpectedConditions.elementToBeClickable(By.id(id)));
+        driver.findElement(By.id(id)).click();
+    }
+
     public void delete() {
-        //Delete
         driver.findElement(By.xpath("//*[@id=\"js-repo-pjax-container\"]/div[1]/nav/ul/li[9]/a/span[1]")).click();
         driver.findElement(By.xpath("//*[@id=\"options_bucket\"]/div[10]/ul/li[4]/details/summary")).click();
-        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
     }
 
     public void submitDelete() {
-        //Submit delete
         driver.findElement(By.xpath("//*[@id=\"options_bucket\"]/div[10]/ul/li[4]/details/details-dialog/div[3]/form/p/input")).sendKeys(
                 driver.findElement(By.xpath("//*[@id=\"options_bucket\"]/div[10]/ul/li[4]/details/details-dialog/div[3]/p[2]/strong")).getText());
 
-        submit("//*[@id=\"options_bucket\"]/div[10]/ul/li[4]/details/details-dialog/div[3]/form/button/span[1]");
+        submit("//*[@id=\"options_bucket\"]/div[10]/ul/li[4]/details/details-dialog/div[3]/form/button/span[1]",
+                checkTextareaNotEmptyByName("verify"));
     }
 
     public void close() {
         driver.close();
     }
 
-//    public boolean checkTextareaNotEmpty(String id) {
-//        WebElement textarea = driver.findElement(By.id(id));
-//        String textInsideTextarea = textarea.getAttribute("value");
-//
-//        if (driver.findElement(By.id(id)))
-//    }
+    public boolean checkTextareaNotEmptyById(String id) {
+        if(driver.findElement(By.id(id)).getAttribute("value").isEmpty()) return false;
+            else return true;
+    }
+
+    public boolean checkTextareaNotEmptyByName(String name) {
+        if(driver.findElement(By.name(name)).getAttribute("value").isEmpty()) return false;
+        else return true;
+    }
+
+    public void writeText(String xpath, String text) {
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+        driver.findElement(By.xpath(xpath)).sendKeys(text);
+    }
 }
